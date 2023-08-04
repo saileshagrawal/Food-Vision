6b1322a26ed05cfb6f42841165f5d101938d5399
saileshagrawal <saileshagarwal9841@gmail.com>
2023-08-04 11:18:37 +0530

    cache_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import altair as alt
 from utils import load_and_prep, get_classes
 
-@st.cache(suppress_st_warning=True)
+@st.cache_data(suppress_st_warning=True)
 def predicting(image, model):
     image = load_and_prep(image)
     image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
